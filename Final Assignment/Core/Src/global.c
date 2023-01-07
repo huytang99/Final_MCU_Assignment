20bf6461fedea2ce0e7b25cc2694389b54d900e8
khanhton2001 <58041151+khanhton2001@users.noreply.github.com>
2023-01-07 09:39:34 +0700

    Update global.c

--- Final Assignment/Core/Src/global.c
+++ Final Assignment/Core/Src/global.c
@@ -9,7 +9,9 @@ int status = 0;
 int pedsStatus = 0;
 int numberCounter = 0;
 
-
+int redDuration1 = 5;
+int greenDuration1 = 3;
+int yellowDuration1 = 2;
 
 int redDuration2 = 5;
 int greenDuration2 = 3;
