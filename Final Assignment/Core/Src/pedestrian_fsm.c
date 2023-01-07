ff600c92f70f85b6fe61b925c01d8d2b4d61c187
khanhton2001 <58041151+khanhton2001@users.noreply.github.com>
2023-01-07 09:54:31 +0700

    Update pedestrian_fsm.c

--- Final Assignment/Core/Src/pedestrian_fsm.c
+++ Final Assignment/Core/Src/pedestrian_fsm.c
@@ -10,10 +10,7 @@ int magnitude = 0;
 
 void pedestrian_run(){
 	switch(pedsStatus) {
-	case NO_PED:
-		turnOffPeds();
-		__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1,0);
-		break;
+	
 	case PED_RED:
 		__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1,0);
 		break;
