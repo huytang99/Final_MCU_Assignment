5882e54986b74d547e7f49d48d4b970607cb0f48
khanhton2001 <58041151+khanhton2001@users.noreply.github.com>
2023-01-07 09:48:50 +0700

    Update pedestrian_fsm.c

--- Final Assignment/Core/Src/pedestrian_fsm.c
+++ Final Assignment/Core/Src/pedestrian_fsm.c
@@ -18,6 +18,18 @@ void pedestrian_run(){
 		__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1,0);
 		break;
 	
+	case PED_GREEN:
+		if(isButtonPressed(0)){
+			if(status == MAN_RED_GREEN || status == MAN_RED_YELLOW){
+				magnitude = 1000;
+				__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1,1000);
+				break;
+			}
+			magnitude = 1000 - 1000*(numberCounter/(double)redDuration1);
+			__HAL_TIM_SetCompare(&htim3, TIM_CHANNEL_1,magnitude);
+		}
+		break;
+	
 	default:
 		break;
 	}
